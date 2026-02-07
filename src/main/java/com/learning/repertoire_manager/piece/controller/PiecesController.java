941c260b74ff5ab8224546e08cd62ac4710f6c8a
jambulud <jambulud@gmail.com>
2026-02-07 19:11:28 +0100

    Remove uused import

--- src/main/java/com/learning/repertoire_manager/piece/controller/PiecesController.java
+++ src/main/java/com/learning/repertoire_manager/piece/controller/PiecesController.java
@@ -9,7 +9,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
