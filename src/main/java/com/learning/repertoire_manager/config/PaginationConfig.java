84020a3f5cea372c69d467a17147b9879c080e43
jambulud <jambulud@gmail.com>
2026-02-25 11:51:59 +0100

    Remove warning

---
+++ src/main/java/com/learning/repertoire_manager/config/PaginationConfig.java
@@ -0,0 +1,11 @@
+package com.learning.repertoire_manager.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
+public class PaginationConfig {
+    // This config ensures PageImpl is wrapped in PagedModel
+}
